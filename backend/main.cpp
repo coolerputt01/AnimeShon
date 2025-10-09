60cc189dc762b5f4b3590d95e969bddcdc4c10c6
coolerputt01 <olumideadekolu9@gmail.com>
2025-10-09 16:52:57 +0100

    Finished the backend script.\n

--- backend/main.cpp
+++ backend/main.cpp
@@ -4,9 +4,16 @@
 #include<unistd.h>
 #include<cstring>
 #include<curl/curl.h>
+#include<map>
+#include<sstream>
+#include<vector>
 using namespace std;
 
 const string animeApiUrl = "https://api.jikan.moe/v4/anime/";
+const string animerandomAPIurl = "https://api.jikan.moe/v4/random/anime";
+const string animeSearchAPIurl = "https://api.jikan.moe/v4/anime?q=";
+const string animeSearchPagePaginationAPIurl = "&page=";
+const string animeSearchLimitPaginationAPIurl = "&limit=10";
 std::string response;
 
 size_t chunkDataAssignment(void* contents, size_t byte, size_t numElem,std::string* output){
@@ -35,7 +42,6 @@ void fetchAnimeData(string apiUrl){
       cerr<<"Failed to form a valid resquest on curl: '"<<curl_easy_strerror(res)<<"'\n";
     }else {
       response = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n" + response;
-      cout<<"Successful Request.\n"<<"Response data: \n"<<response;
     }
     curl_easy_cleanup(curl);
   }else {
@@ -54,7 +60,7 @@ int setUpServer(int &sockets) {
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(8080);
   
-  if(bind(sockets,(struct sockaddr*)&address,sizeof(address)) < 0){
+  if(::bind(sockets,(struct sockaddr*)&address,sizeof(address)) < 0){
     cerr<<"Failed to bind at address on PORT: 8080\n";
     return 1;
   }
@@ -64,6 +70,32 @@ int setUpServer(int &sockets) {
   }
   cout<<"Server running on PORT: 8080......\n";
   return 0;
+}
+
+map<string, string> parseQuery(string &requestParamStr){
+  map<string, string> param;
+  stringstream ss(requestParamStr);
+  string pair;
+  while(getline(ss,pair,'&')){
+    auto equator = pair.find('=');
+    if(equator != string::npos){
+      param[pair.substr(0,equator)] = pair.substr(equator + 1);
+    }
+  }
+  return param;
+}
+
+vector<string> parseStreamUrl(string &url){
+  vector<string> parts;
+  string part;
+  stringstream ss(url);
+  
+  while(getline(ss,part,'/')){
+    if(!part.empty()){
+      parts.push_back(part);
+    }
+  }
+  return parts;
 }
 
 void handleRequest(int &sockets,string apiUrl){
@@ -82,15 +114,63 @@ void handleRequest(int &sockets,string apiUrl){
     }
     
     string request(buffer);
-    if(request.find("GET /anime") != string::npos){
+    if(request.find("GET /anime") != string::npos && request.find("/episodes/") != string::npos){
+    string animeUrl = request;
+    vector<string> animeIdParts = parseStreamUrl(animeUrl);
+    string newApiUrl = animeApiUrl + animeIdParts[2] + "/episodes/" + animeIdParts[4];
+    for(auto &t : animeIdParts){
+        cout<<t<<endl;
+    }
+    fetchAnimeData(newApiUrl);
+    }else if(request.find("GET /anime") != string::npos && request.find("/episodes") != string::npos){
+          string animeUrl = request;
+          vector<string> animeIdParts = parseStreamUrl(animeUrl);
+          string newApiUrl = animeApiUrl + animeIdParts[2] + "/episodes";
+          fetchAnimeData(newApiUrl);
+    }else if(request.find("GET /anime/") == 0){
+          string animeUrl = request;
+          vector<string> idParts = parseStreamUrl(animeUrl);
+          for(auto &t : idParts){
+            cout<<t<<endl;
+          }
+          auto animeId = idParts[2];
+          string newApiUrl = animeApiUrl + animeId;
+          fetchAnimeData(newApiUrl);
+        }else if(request.find("GET /anime") == 0){
+      cout<<"lollllll\n";
       fetchAnimeData(apiUrl);
+        }else if(request.find("GET /random-anime") != string::npos){
+          fetchAnimeData(animerandomAPIurl);
+        }else if(request.find("GET /") != string::npos && request.find("/streaming") != string::npos) {
+          string streamingApiUrl = request;
+          vector<string> animeIdParts = parseStreamUrl(streamingApiUrl);
+          for(auto &t : animeIdParts){
+            cout<<t<<endl;
+          }
+          auto animeId = animeIdParts[1];
+          string newApiUrl = animeApiUrl + animeId + "/streaming";
+          fetchAnimeData(newApiUrl);
+        }else if(request.find("GET /search") != string::npos){
+          
+          size_t requestPath = request.find("GET /search");
+          size_t requestQueryStart = request.find('?',requestPath);
+          if(requestQueryStart == string::npos){
+            cerr<<"No query params found.\n";
+            continue;
+          }
+          size_t requestQueryEnd = request.find(" ",requestQueryStart);
+          
+          std::string queryParamsStr = request.substr(requestQueryStart + 1,requestQueryEnd - requestQueryStart- 1);
+          
+          auto params = parseQuery(queryParamsStr);
+          string apiUrl = animeSearchAPIurl + params["q"] + animeSearchPagePaginationAPIurl + params["page"] + animeSearchLimitPaginationAPIurl;
+          fetchAnimeData(apiUrl);
         }else if(request.find("GET /") != string::npos){
           response = "HTTP/1.1 200 OK\r\n"
                "Content-Type: text/html\r\n"
                "Connection: close\r\n\r\n"
                "<h1>Hey there</h1>\n";
-        }
-        else {
+        }else {
             response = "HTTP/1.1 404 Not Found\r\n"
                        "Content-Type: text/plain\r\n"
                        "Connection: close\r\n\r\n"
